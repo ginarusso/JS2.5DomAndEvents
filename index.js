430463ce52bff84eae59cf1c26ed883bed293764
Gina Russo <gina@Ginas-Air.fios-router.home>
2023-06-20 21:37:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const containerEl = document.querySelector('body')
+// const add = document.querySelector('#addition')
+// const subtract = document.querySelector('#subtraction')
+// const multiply = document.querySelector('#multiplication')
+// const divide = document.querySelector('#division')
+let num1El = document.getElementById('num1')
+let num2El = document.getElementById('num2')
+let result = document.querySelector('#result')
+// let resultEl = document.getElementById('result')
+let answer = 0
+result.innerText = 0
+// function reset() {
+//     console.log("clicked")
+//     result.innerText = 0
+// }
+
+containerEl.addEventListener('click', function (e) {
+    // console.log("clicked")
+    // console.log(e.target.dataset.addNumbers)
+    let num1 = parseInt(num1El.value)
+    let num2 = parseInt(num2El.value)
+    const target = e.target
+    const addNums = target.dataset.addNumbers
+    const subtractNums = target.dataset.subtractNumbers
+    const multiplyNums = target.dataset.multiplyNumbers
+    const divideNums = target.dataset.divideNumbers
+    const moduloNums = target.dataset.moduloNumbers
+    // const reset = target.dataset.reset
+
+    if (addNums) {
+        answer = num1 + num2
+        result.innerText = answer
+    } else if (subtractNums) {
+        answer = num1 - num2
+        result.innerText = answer
+    } else if (multiplyNums) {
+        answer = num1 * num2
+        result.innerText = answer
+    } else if (divideNums) {
+        answer = num1 / num2
+        result.innerText = answer
+    } else if (moduloNums) {
+        answer = num1 % num2
+        result.innerText = answer
+    } else {
+        result.innerText = 0
+    }
+} )
+
+//refactored: removed onclick from HTML
+// function compute() {
+//     // const num1 = parseInt(num1El.value)
+//     // const num2 = parseInt(num2El.value)
+//     if (add) {
+//         console.log("add")
+//         answer = num1 + num2
+//         resultEl.innerText = answer
+//     } else if (subtract) {
+//         console.log("subtract")
+//         answer = num1 - num2
+//         resultEl.innerText = answer
+//         console.log(answer)
+//     }
+// }
+
+// function add() {
+//     // console.log("add clicked")
+//     console.log(num1El.value)
+//     console.log(num2El.value)
+//     const num1 = parseInt(num1El.value)
+//     const num2 = parseInt(num2El.value)
+//     // console.log(num1 + num2)
+//     answer = num1 + num2
+//     resultEl.innerText = answer
+// }
+
+// function subtract() {
+//     // console.log("subtract clicked")
+//     // console.log(num1El.value)
+//     // console.log(num2El.value)
+//     const num1 = parseInt(num1El.value)
+//     const num2 = parseInt(num2El.value)
+//     // console.log(num1 - num2)
+//     answer = num1 - num2
+//     resultEl.innerText = answer
+// }
+
+// function multiply() {
+//     // console.log("multiply clicked")
+//     // console.log(num1El.value)
+//     // console.log(num2El.value)
+//     const num1 = parseInt(num1El.value)
+//     const num2 = parseInt(num2El.value)
+//     // console.log(num1 * num2)
+//     answer = num1 * num2
+//     resultEl.innerText = answer
+// }
+
+// function divide() {
+//     // console.log("divide clicked")
+//     // console.log(num1El.value)
+//     // console.log(num2El.value)
+//     const num1 = parseInt(num1El.value)
+//     const num2 = parseInt(num2El.value)
+//     // console.log(num1 / num2)
+//     answer = num1 / num2
+//     resultEl.innerText = answer
+//  }
